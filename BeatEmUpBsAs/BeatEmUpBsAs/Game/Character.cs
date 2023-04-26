26ec22e97d9ed8f925f1387f3acc2492c65fdad4
Maximo Samperio <maximosamperio01@gmail.com>
2023-04-25 21:33:49 -0300

    Added Menus and Updated Animation code

--- BeatEmUpBsAs/BeatEmUpBsAs/Game/Character.cs
+++ BeatEmUpBsAs/BeatEmUpBsAs/Game/Character.cs
@@ -9,12 +9,15 @@ namespace Game
     public class Character
     {
         // Character Properties
-        public Transform _transform;       // Transform of the character
-        public Renderer _renderer;         // Render of the character
+        private Transform _transform;       // Transform of the character
+        public Transform Transform => _transform;
+
+        private Renderer _renderer;         // Render of the character
+        public Renderer Renderer => _renderer;
+
 
         // Movement related variables
         private float _movementSpeed;
-        private bool _isMoving;
         private bool _kick;
 
         private Animation idleAnimation;
@@ -50,26 +53,24 @@ namespace Game
 
             // Running animation
             List<Texture> walkTextures = new List<Texture>();          
-            if (_isMoving == true)
-            {
+            
                 for (int i = 1; i < 5; i++)                                 
                 {
                     Texture frame = Engine.GetTexture($"Textures/BG/WalkAnim/{i}.png");     
                     walkTextures.Add(frame);                                
                 }
-            }
+  
             walkAnimation = new Animation("Walk", walkTextures, 0.1f, true);
 
             // Kick Animation
             List<Texture> kickTextures = new List<Texture>();          
-            if (_kick == true)
-            {
+            
                 for (int i = 1; i < 6; i++)                                 
                 {
                     Texture frame = Engine.GetTexture($"Textures/BG/KickAnim/{i}.png");     
                     kickTextures.Add(frame);                               
                 }
-            }
+
             kickAnimation = new Animation("Kick", kickTextures, 0.1f, true);
 
         }
@@ -120,52 +121,73 @@ namespace Game
             if (Engine.GetKey(Keys.W))
             {
                 MoveUp();
-                bool _isMoving = true;
+                currentAnimation = walkAnimation;            
             }
 
             // Checks for the S key for movement
             if (Engine.GetKey(Keys.S))
             {
                 MoveDown();
-                bool _isMoving = true;
+                currentAnimation = walkAnimation;
             }
 
             // Checks for the A key for movement
             if (Engine.GetKey(Keys.A))
             {
                 MoveLeft();
-                bool _isMoving = true;
+                currentAnimation = walkAnimation;
             }
 
             // Checks for the D key for movement
             if (Engine.GetKey(Keys.D))
             {
                 MoveRight();
-                bool _isMoving = true;
+                currentAnimation = walkAnimation;
             }
 
             // Checks for the F key to kick
             if (Engine.GetKey(Keys.F))
             {
                 Kick();
-                bool _kick = true;
+                currentAnimation = kickAnimation;
             }
         }
 
         // Moves the character up
-        private void MoveUp() => _transform.Translate(new Vector2(0, -1), _movementSpeed);
+        private void MoveUp()
+        {
+            _transform.Translate(new Vector2(0, -1), _movementSpeed);
+            currentAnimation = walkAnimation;
+
+        }
 
         // Moves the character down
-        private void MoveDown() => _transform.Translate(new Vector2(0, 1), _movementSpeed);
+        private void MoveDown()
+        {
+            _transform.Translate(new Vector2(0, 1), _movementSpeed);
+            currentAnimation = walkAnimation;
+        }
 
         // Moves the character to the left
-        private void MoveLeft() => _transform.Translate(new Vector2(-1, 0), _movementSpeed);
+        private void MoveLeft()
+        {
+            _transform.Translate(new Vector2(-1, 0), _movementSpeed);
+            currentAnimation = walkAnimation;
+        }
 
         // Moves the character to the right
-        private void MoveRight() => _transform.Translate(new Vector2(1, 0), _movementSpeed);
+        private void MoveRight()
+        {
+            _transform.Translate(new Vector2(1, 0), _movementSpeed);
+            currentAnimation = walkAnimation;
+        }
 
         // Makes the character throw a front kick
-        private void Kick() => _kick = true;
+        private void Kick()
+        {
+            _kick = true;
+            currentAnimation = kickAnimation;
+        }
 
         #endregion
     }
