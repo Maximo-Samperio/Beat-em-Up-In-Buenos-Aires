1595f95ffe9e1cb779759562eef0aca8254a5963
Maximo Samperio <maximosamperio01@gmail.com>
2023-06-26 12:04:14 -0300

    Detection of player attack

---
+++ BeatEmUpBsAs/BeatEmUpBsAs/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Game;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 3;
+            int y = 4;
+
+            var vector = new Vector2(x, y);
+
+            var ExpectedResult = $"X : {x} / Y : {y}";
+
+            var Result = vector.ToString();
+
+            Assert.AreEqual(ExpectedResult, Result);
+        }
+    }
+}
---
+++ BeatEmUpBsAs/BeatEmUpBsAs/UnitTestProject1/UnitTest2.cs
@@ -0,0 +1,17 @@
+﻿using Game;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+    }
+}
