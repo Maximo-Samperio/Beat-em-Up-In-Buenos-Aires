e74b5c1757a3e38369aec3d2a36c3a6a75cb1322
Stetik <marcoscaceres697@gmail.com>
2023-06-26 16:07:18 -0300

    fail to unit test!

---
+++ BeatEmUpBsAs/BeatEmUpBsAs/UT2/UnitTest2.cs
@@ -0,0 +1,29 @@
+﻿using Game;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net.Mime;
+
+namespace UT2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int positionX = 10;
+            int positionY = 20;
+      
+
+            var vector = new Vector2(positionX, positionY);
+
+            var expectedresult = $"Position - X : {positionX} / Y : {positionY}\n";
+
+            var result = vector.ToString();
+
+            Assert.AreEqual(expectedresult, vector);
+
+           
+        }
+    }
+}
